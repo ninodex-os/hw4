c9a4a2f1713b3f33c6c25c59ccddb9e1d78785c4
Luis Gutierrez <luisg581@usc.edu>
2025-04-06 05:41:03 +0000

    version 3

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,12 +1,10 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-#include "equal-paths.h"
-using namespace std;
 #endif
 
 
-
-
+#include "equal-paths.h"
+using namespace std;
 
 // You may add any prototypes of helper functions here
 
